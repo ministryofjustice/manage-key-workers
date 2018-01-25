095064a712d651f9f7c34fc60dbd790c42668f0e
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2018-01-25 10:10:29 +0000

    code share

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 
 class Login extends Component {
@@ -7,34 +7,50 @@ class Login extends Component {
     this.state = {
       error: null,
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+
   }
-  componentDidMount(){
-     axios.post('/login', {
-       username: 'username',
-       password: 'password'
-     }).then(data => {
-       this.props.onLogin(data.headers['jwt'], this.props.history);
-     })
-     .catch(error => {
-       this.setState({
-         error: error.response.data || 'Something went wrong',
-       })
-     })
+
+  componentDidMount() {
+  }
+
+  handleSubmit(event) {
+    axios.post('/login', {
+      username: 'itag_user',
+      password: 'password2'
+    }).then(data => {
+      this.props.onLogin(data.headers['jwt'], this.props.history);
+    })
+      .catch(error => {
+        this.setState({
+          error: error.response.data || 'Something went wrong',
+        })
+      })
+    event.preventDefault();
   }
 
   render() {
     return (
-      <div> 
-        <h1 className="heading-large"> Login  </h1>   
-        
-        {this.state.error && 
+      <div>
+        <h1 className="heading-large"> Login </h1>
+
+        {this.state.error &&
         <div className="error-summary">
           <div className="error-message">
-             <div> {this.state.error} </div>
+            <div> {this.state.error} </div>
           </div>
         </div>}
-
+        <div>
+          <form>
+            <label class="form-label" for="full-name-f1">Username</label>
+            <input class="form-control" id="full-name-f1" type="text" name="username"/>
+            <label class="form-label" for="password">Password</label>
+            <input class="form-control" id="password" type="password" name="password"/>
+            <input type="submit" value="Sign in" onSubmit={this.handleSubmit}/>
+          </form>
+        </div>
       </div>
+
     );
   }
 }
