14494a433e16eaf743a2145c06ba33d9113cd839
steverendell <steve.rendell@syscon.net>
2018-01-29 12:17:14 +0000

    Some css and correct merge error

--- src/Unallocated.js
+++ src/Unallocated.js
@@ -33,31 +33,28 @@ class Unallocated extends Component {
           <td>csra todo</td>
         </tr>
     )});
-        <tr>
-        <td>{a.lastName}, {a.firstName}</td>
-        <td>{a.offenderNo}</td>
-        <td>{a.internalLocationDesc}</td>
-        <td>release date todo</td>
-        <td>csra todo</td>
-        </tr>
-    )});
     return (
       <div>
-        <h1 className="heading-large">Prisoners without key workers</h1>
-        <p>These prisoners have not yet been allocated a Key worker. Use the button below to automatically allocate them to one. You can override the selections on the next page if you wish.</p>
-        <table>
-          <thead>
-          <tr>
-            <th>Name</th>
-            <th>NOMS ID</th>
-            <th>Location</th>
-            <th>RD</th>
-            <th>CSRA</th>
-          </tr>
-          </thead>
-          <tbody>{offenders}</tbody>
-        </table>
-        <button className="button pure-u-md-2-12" onClick={() => this.jumpTo('asdf')}>Allocate</button>
+        <div className="pure-u-md-8-12">
+          <h1 className="heading-large">Prisoners without key workers</h1>
+          <p>These prisoners have not yet been allocated a Key worker. Use the button below to automatically allocate
+            them to one. You can override the selections on the next page if you wish.</p>
+          <table>
+            <thead>
+            <tr>
+              <th>Name</th>
+              <th>NOMS ID</th>
+              <th>Location</th>
+              <th>RD</th>
+              <th>CSRA</th>
+            </tr>
+            </thead>
+            <tbody>{offenders}</tbody>
+          </table>
+        </div>
+        <div>
+          <button className="button pure-u-md-2-12" onClick={() => this.jumpTo('asdf')}>Allocate</button>
+        </div>
       </div>
     )
   }
