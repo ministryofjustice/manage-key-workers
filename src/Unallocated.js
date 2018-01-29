6f54d9d70aa74b1fbe7031de2442585b7c50a439
steverendell <steve.rendell@syscon.net>
2018-01-29 11:17:56 +0000

    Add allocation component skeleton

--- src/Unallocated.js
+++ src/Unallocated.js
@@ -13,9 +13,9 @@ class Unallocated extends Component {
       headers: {
         jwt: this.props.jwt
       }
-    }).then(data => {
-      console.log('data from api call ' + data);
-      this.props.setUnallocatedOffenders(data);
+    }).then(response => {
+      console.log('data from api call ' + response);
+      this.props.setUnallocatedOffenders(response.data);
     })
       .catch(error => {
         this.props.displayError(error);
@@ -25,11 +25,30 @@ class Unallocated extends Component {
   render () {
     const offenders = this.props.list.map((a) => {
       return (
-        <li>{a}</li>);
-    });
+        <tr>
+        <td>{a.lastName}, {a.firstName}</td>
+        <td>{a.offenderNo}</td>
+        <td>{a.internalLocationDesc}</td>
+        <td>release date todo</td>
+        <td>csra todo</td>
+        </tr>
+    )});
     return (
       <div>
-        <ol>{offenders}</ol>
+        <h1>Prisoners without key workers</h1>
+        <div>These prisoners have not yet been allocated a Key worker. Use the button below to automatically allocate them to one. You can override the selections on the next page if you wish.</div>
+        <table>
+          <thead>
+          <tr>
+            <th>Name</th>
+            <th>NOMS ID</th>
+            <th>Location</th>
+            <th>RD</th>
+            <th>CSRA</th>
+          </tr>
+          </thead>
+          <tbody>{offenders}</tbody>
+        </table>
         <button onClick={() => this.jumpTo('asdf')}>Allocate</button>
       </div>
     )
