015553b290c7500cd701ef4f4e7d068751329ab5
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2018-02-21 13:00:42 +0000

    added configurable display time and test to Messagebar component (#48)

--- src/MessageBar/messageBar.test.js
+++ src/MessageBar/messageBar.test.js
@@ -8,12 +8,21 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('ManualAllocation component', () => {
   it('should render message correctly', async () => {
-    const component = shallow(<MessageBar message="Hello!"/>);
+    const component = shallow(<MessageBar message="Hello!" clearMessage={jest.fn()}/>);
     expect(component.find('div').at(1).text()).toEqual('Hello!');
   });
 
+  it('should clear message after displayTime expires', async (done) => {
+    const clearMessageMock = jest.fn();
+    shallow(<MessageBar message="Hello!" displayTime={100} clearMessage={clearMessageMock}/>);
+    setTimeout(function () {
+      expect(clearMessageMock.mock.calls.length).toBe(1);
+      done();
+    }, 200);
+  });
+
   it('should not render message bar if no message provided', async () => {
-    const component = shallow(<MessageBar message=""/>);
+    const component = shallow(<MessageBar message="" clearMessage={jest.fn()}/>);
     expect(component.find('div').exists()).toEqual(false);
   });
 });
