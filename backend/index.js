98cbb363d04ab14096fd1ef3656ec1b453ab45e6
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2018-02-22 08:58:17 +0000

    NN-534 Dev env 500/ETIMEOUT errors fixing 3 (#52)

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,3 @@
-process.env.UV_THREADPOOL_SIZE = 100;
-
 require('dotenv').config();
 
 // Do appinsights first as it does some magic instrumentation work, i.e. it affects other 'require's
--- backend/logError.js
+++ backend/logError.js
@@ -13,7 +13,8 @@ const logError = (url, error, msg) => {
   } else {
     log.error({
       url,
-      code: error.code
+      code: error.code,
+      message: error.message
     }, msg);
   }
 };
