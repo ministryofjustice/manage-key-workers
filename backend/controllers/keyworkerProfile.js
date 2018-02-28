808291fe9f138ce4f73890e141477ae5a0182c7c
steverendell <steve.rendell@syscon.net>
2018-02-28 16:53:44 +0000

    NN-568 Wire up KW profile and allocations

--- backend/controllers/keyworkerProfile.js
+++ backend/controllers/keyworkerProfile.js
@@ -1,23 +1,11 @@
 const express = require('express');
 const router = express.Router();
-// const elite2Api = require('../elite2Api');
+const elite2Api = require('../elite2Api');
 const asyncMiddleware = require('../middleware/asyncHandler');
 
 router.get('/', asyncMiddleware(async (req, res) => {
-  // const response = await elite2Api.keyworkerSearch(req);
-  res.json(createTestDataResponse().data);
+  const response = await elite2Api.keyworker(req);
+  res.json(response.data);
 }));
-
-function createTestDataResponse () {
-  return {
-    data: {
-      staffId: 2,
-      firstName: 'Amy',
-      lastName: 'Ronson',
-      status: "active",
-      currentRole: "Key worker2"
-    }
-  };
-}
 
 module.exports = router;
