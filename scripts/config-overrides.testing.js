17681cf187299b8b6d8f8cb30f04df0071ac9896
steverendell <steve.rendell@syscon.net>
2018-03-09 15:06:21 +0000

    Configure test coverage

--- scripts/config-overrides.testing.js
+++ scripts/config-overrides.testing.js
@@ -1,6 +1,8 @@
 
+module.exports = function (config) {
+  config.testMatch.push('<rootDir>/backend/tests/**/?(*.)(spec|test).{js,jsx,mjs}');
+  config.collectCoverageFrom.push('src/**/*.js');
+  config.collectCoverageFrom.push('backend/**/*.js');
 
-module.exports = function(config){
-    config.testMatch.push('<rootDir>/backend/tests/**/?(*.)(spec|test).{js,jsx,mjs}');
-    return config;
-}
+  return config;
+};
