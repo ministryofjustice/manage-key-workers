0eee98da7f142318478860bd4159ae74f04c2ee8
Peter Bell <peter.bell@syscon.net>
2018-03-26 10:35:42 +0100

    Use the Gradle webdriverBinaries plugin. Work in progress...

--- keyworker-specs/src/test/resources/GebConfig.groovy
+++ keyworker-specs/src/test/resources/GebConfig.groovy
@@ -1,20 +1,35 @@
 import org.openqa.selenium.Dimension
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
 
 waiting {
     timeout = 2
 }
 
-driver = {
+environments {
+    chrome {
+        driver = { new ChromeDriver() }
+    }
 
-    System.setProperty('webdriver.chrome.driver', '/Users/syscon/dev/work/licences/chromedriver')
-
-    def driverInstance = new ChromeDriver()
-    driverInstance.manage().window().size = new Dimension(640, 1134)
-    driverInstance
+    chromeHeadless {
+        driver = {
+            ChromeOptions options = new ChromeOptions()
+            options.addArguments('headless')
+            new ChromeDriver(options)
+        }
+    }
 }
+//
+//driver = {
+//
+//    System.setProperty('webdriver.chrome.driver', '/Users/syscon/dev/work/licences/chromedriver')
+//
+//    def driverInstance = new ChromeDriver()
+//    driverInstance.manage().window().size = new Dimension(640, 1134)
+//    driverInstance
+//}
 
 
 baseUrl = "http://localhost:3001/"
 
-reportsDir = "build/geb-reports"
+//reportsDir = "build/geb-reports"
