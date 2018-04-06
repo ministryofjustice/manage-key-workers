a8b87290b07321940aed10ac775dfe1cef838520
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2018-04-06 16:35:37 +0100

    NN-662 Customise health check to provide similar info to new nomis API (#117)
    * NN-662 Customise health check to provide similar info to new nomis API
    
    * NN-662 Customise health check to provide similar info to new nomis API
    
    Remove express-healthcheck and lint error
    
    * NN-662 Customise health check to provide similar info to new nomis API
    
    Integration tests
    
    * NN-662 Customise health check to provide similar info to new nomis API
    
    Integration tests

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 require('./azure-appinsights');
 
 const path = require('path');
-const fs = require('fs');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
@@ -28,6 +27,7 @@ const keyworkerProfile = require('./controllers/keyworkerProfile');
 const keyworkerUpdate = require('./controllers/keyworkerUpdate');
 const userMe = require('./controllers/userMe');
 const getConfig = require('./controllers/getConfig');
+const health = require('./controllers/health');
 
 const log = require('./log');
 const config = require('./config');
@@ -45,17 +45,6 @@ const sessionConfig = {
   expires: new Date(Date.now() + sessionExpiryMinutes),
   maxAge: sessionExpiryMinutes
 };
-
-function getAppInfo () {
-  const packageData = JSON.parse(fs.readFileSync('./package.json'));
-  const buildVersion = fs.existsSync('./build-info.json') ? JSON.parse(fs.readFileSync('./build-info.json')).buildNumber : packageData.version;
-
-  return {
-    name: packageData.name,
-    version: buildVersion,
-    description: packageData.description
-  };
-}
 
 app.set('trust proxy', 1); // trust first proxy
 
@@ -88,11 +77,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/health', require('express-healthcheck')());
-
-app.use('/info', (req, res) => {
-  res.json(getAppInfo());
-});
+app.use('/health', health);
+app.use('/info', health);
 
 app.use(express.static(path.join(__dirname, '../public'), { index: 'dummy-file-which-doesnt-exist' })); // TODO: setting the index to false doesn't seem to work
 app.use(express.static(path.join(__dirname, '../build'), { index: 'dummy-file-which-doesnt-exist' }));
@@ -123,5 +109,5 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 3001;
 
 app.listen(port, function () {
-  console.log('Backend running on port %s', port);
+  console.log('Backend running on port', port);
 });
--- keyworker-specs/src/test/groovy/uk/gov/justice/digital/hmpps/keyworker/specs/HealthSpecification.groovy
+++ keyworker-specs/src/test/groovy/uk/gov/justice/digital/hmpps/keyworker/specs/HealthSpecification.groovy
@@ -1,24 +1,54 @@
 package uk.gov.justice.digital.hmpps.keyworker.specs
 
+import groovyx.net.http.HttpException
+import org.junit.Rule
 import spock.lang.Specification
+import uk.gov.justice.digital.hmpps.keyworker.mockapis.KeyworkerApi
 
 import static groovyx.net.http.HttpBuilder.configure
 
 class HealthSpecification extends Specification {
 
+    @Rule
+    KeyworkerApi keyworkerApi = new KeyworkerApi()
 
-    def "Health page reports application uptime"() {
+    def "Health page reports ok"() {
+
+        keyworkerApi.stubHealth('/health')
 
         def http = configure {
             request.uri = 'http://localhost:3001/health'
         }
 
         when:
-        def response = http.get() {
+        def response = http.get() {}
+
+        then:
+        response.uptime > 0.0
+        response.name == "keyworker-ui"
+        !response.version.isEmpty()
+        response.api.status == "UP"
+        response.api.db.status == "UP"
+    }
+
+    def "Health page reports API unhealthy"() {
+
+        keyworkerApi.stub500Error('/health')
+
+        def http = configure {
+            request.uri = 'http://localhost:3001/health'
+        }
+
+        when:
+        def response
+        try {
+            http.get() {}
+        } catch (HttpException e) {
+            response = e.body
         }
 
         then:
         response.uptime > 0.0
+        response.api == "Request failed with status code 500"
     }
-
 }
