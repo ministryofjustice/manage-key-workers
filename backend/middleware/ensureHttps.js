337a4bc98fdc52f4dba1a5218cdd42514c5cf7b7
JamieDraperUK <draper65@googlemail.com>
2018-04-24 13:56:48 +0100

    Moved the health and info handlers to come before the https redirect

--- backend/middleware/ensureHttps.js
+++ backend/middleware/ensureHttps.js
@@ -1,7 +1,8 @@
 const log = require('../log');
 
 module.exports = function ensureSec (req, res, next) {
-  if (req.secure) {
+  if (req.secure ) {
+    console.error('NEXT NEXT NEXT NEXT NEXTR');
     return next();
   }
   const redirectUrl = "https://" + req.headers.hostname + req.url;
