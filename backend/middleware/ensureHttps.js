240bce1969a29d53da2cbb233acce0684202a228
JamieDraperUK <draper65@googlemail.com>
2018-04-24 11:41:03 +0100

    added logging and used config to check for production

--- backend/middleware/ensureHttps.js
+++ backend/middleware/ensureHttps.js
@@ -1,7 +1,11 @@
+const log = require('../log');
 
 module.exports = function ensureSec (req, res, next) {
   if (req.headers["x-forwarded-proto"] === "https") {
     return next();
   }
+  const redirectUrl = "https://" + req.headers.host + req.url;
+  log.info(`Redirecting to ${redirectUrl}`);
+
   res.redirect("https://" + req.headers.host + req.url);
 };
