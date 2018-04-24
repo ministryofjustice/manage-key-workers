b861637b1e62feadf1a7c628c44840c80c6778ce
JamieDraperUK <draper65@googlemail.com>
2018-04-24 14:44:17 +0100

    remove console.log and fix req.hostname

--- backend/middleware/ensureHttps.js
+++ backend/middleware/ensureHttps.js
@@ -1,11 +1,10 @@
 const log = require('../log');
 
 module.exports = function ensureSec (req, res, next) {
-  if (req.secure ) {
-    console.error('NEXT NEXT NEXT NEXT NEXTR');
+  if (req.secure) {
     return next();
   }
-  const redirectUrl = "https://" + req.headers.hostname + req.url;
+  const redirectUrl = "https://" + req.hostname + req.url;
   log.info(`Redirecting to ${redirectUrl}`);
 
   res.redirect(redirectUrl);
