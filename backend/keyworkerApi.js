98a6c865aecdfac34187f3b6f169bbcd0e094fee
Peter Bell <peter.bell@syscon.net>
2018-05-04 11:55:22 +0100

    NN-725 Include health status of keyworker and elite2Api when reporting health. Remote health checks performed in parallel. Returned HTTP status code is maximum of reported api HTTP status codes.

--- backend/keyworkerApi.js
+++ backend/keyworkerApi.js
@@ -88,12 +88,22 @@ const keyworkerAllocations = (req, res) => gateway.getRequest({
 });
 
 const service = {
-  unallocated, allocated, offenderKeyworkerList, availableKeyworkers, keyworker, allocate, autoallocated,
-  autoAllocate, autoAllocateConfirm, keyworkerAllocations, keyworkerSearch, keyworkerUpdate,
+  unallocated,
+  allocated,
+  offenderKeyworkerList,
+  availableKeyworkers,
+  keyworker,
+  allocate,
+  autoallocated,
+  autoAllocate,
+  autoAllocateConfirm,
+  keyworkerAllocations,
+  keyworkerSearch,
+  keyworkerUpdate,
   keyworkerApiUrl
 };
 
-function encodeQueryString (input) {
+function encodeQueryString(input) {
   return encodeURIComponent(input);
 }
 
