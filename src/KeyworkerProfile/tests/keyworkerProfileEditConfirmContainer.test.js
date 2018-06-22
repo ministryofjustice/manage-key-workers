5e494c7755aca89ebb93f69c7d4fe358adb4c2fc
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2018-06-22 12:44:16 +0100

    bug fixes NN-925 and 926 plus status colours (#190)
    * bug fixes NN-925 and 926 plus status colours
    
    * bug fixes NN-925 and 926 plus status colours
    
    * removing style

---
+++ src/KeyworkerProfile/tests/keyworkerProfileEditConfirmContainer.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { KeyworkerProfileEditConfirmContainer } from "../containers/KeyworkerProfileEditConfirmContainer";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const keyworkerAnnualLeave = {
+  firstName: "Frank",
+  lastName: "Butcher",
+  staffId: 123,
+  status: "UNAVAILABLE_ANNUAL_LEAVE",
+  capacity: 8
+};
+
+const keyworkerActive = {
+  firstName: "Frank",
+  lastName: "Butcher",
+  staffId: 123,
+  status: "INACTIVE",
+  capacity: 8
+};
+
+
+describe('Keyworker Profile Edit component', () => {
+  it('should validate empty choices and date picker for unavailable_annual_leave status', async () => {
+    const resetValidation = jest.fn();
+    const setValidation = jest.fn();
+    const component = shallow(<KeyworkerProfileEditConfirmContainer setValidationErrorDispatch={setValidation} resetValidationErrorsDispatch={resetValidation} keyworker={keyworkerAnnualLeave} status="UNAVAILABLE_ANNUAL_LEAVE" handleSaveChanges={jest.fn()} setStatusChangeBehaviourDispatch={jest.fn()} dateDispatch={jest.fn()} handleCancel={jest.fn()} match={{}} history={{}} handleError={jest.fn()} handleOptionChange={jest.fn()} agencyId="LEI" />);
+    const instance = component.instance();
+    expect(instance.validate()).toBe(false);
+    expect(resetValidation.mock.calls.length).toEqual(1);
+    expect(setValidation.mock.calls.length).toEqual(2);
+  });
+
+  it('should only validate empty choices for INACTIVE status', async () => {
+    const resetValidation = jest.fn();
+    const setValidation = jest.fn();
+    const component = shallow(<KeyworkerProfileEditConfirmContainer setValidationErrorDispatch={setValidation} resetValidationErrorsDispatch={resetValidation} keyworker={keyworkerActive} status="INACTIVE" handleSaveChanges={jest.fn()} setStatusChangeBehaviourDispatch={jest.fn()} dateDispatch={jest.fn()} handleCancel={jest.fn()} match={{}} history={{}} handleError={jest.fn()} handleOptionChange={jest.fn()} agencyId="LEI" />);
+    const instance = component.instance();
+    expect(instance.validate()).toBe(false);
+    expect(resetValidation.mock.calls.length).toEqual(1);
+    expect(setValidation.mock.calls.length).toEqual(1);
+  });
+});
+
