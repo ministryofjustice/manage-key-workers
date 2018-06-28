2c149dac964f68a08cc638691d0a9babb1a6af59
steverendell <steve.rendell@syscon.net>
2018-06-28 12:36:14 +0100

    NN-784 Add 'status' filter to key worker search - test fix

--- src/KeyworkerProfile/components/Status.js
+++ src/KeyworkerProfile/components/Status.js
@@ -7,7 +7,7 @@ class Status extends Component {
     return (<select id="status-select" name="status-select" className="form-control"
       value={this.props.statusValue}
       onChange={this.props.handleStatusChange}>
-      // when this is used for a filter we need an 'all' option
+      {/* When this is used for a filter we need an 'all' option*/}
       {this.props.filter && <option key="" value="">All</option>}
       <option key="ACTIVE" value="ACTIVE">{getStatusDescription('ACTIVE')}</option>
       <option key="INACTIVE" value="INACTIVE">{getStatusDescription('INACTIVE')}</option>
