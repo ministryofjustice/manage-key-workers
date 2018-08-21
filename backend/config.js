26ac09cfbd88385dd4797e1bac91658567b67486
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-08-21 15:14:52 +0100

    Switch to using Oauth URL

--- backend/config.js
+++ backend/config.js
@@ -15,11 +15,15 @@ module.exports = {
     expiryMinutes: process.env.WEB_SESSION_TIMEOUT_IN_MINUTES || 20
   },
   apis: {
+    oauth2: {
+      url: process.env.OAUTH_ENDPOINT_URL || 'http://localhost:8080/',
+      timeoutSeconds: process.env.OAUTH_ENDPOINT_TIMEOUT_SECONDS || 15,
+      clientId: process.env.API_CLIENT_ID || 'elite2apiclient',
+      clientSecret: process.env.API_CLIENT_SECRET || 'clientsecret'
+    },
     elite2: {
       url: process.env.API_ENDPOINT_URL || 'http://localhost:8080/',
       timeoutSeconds: process.env.API_ENDPOINT_TIMEOUT_SECONDS || 15,
-      clientId: process.env.API_CLIENT_ID || 'elite2apiclient',
-      clientSecret: process.env.API_CLIENT_SECRET || 'clientsecret'
     },
     keyworker: {
       url: process.env.KEYWORKER_API_URL || 'http://localhost:8081/',
