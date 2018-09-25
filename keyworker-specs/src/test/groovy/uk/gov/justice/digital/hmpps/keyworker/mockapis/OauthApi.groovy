eeb8a116e2ffdcc26417c56a00cf95824e721c16
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-09-25 15:36:10 +0100

    Fix tests

--- keyworker-specs/src/test/groovy/uk/gov/justice/digital/hmpps/keyworker/mockapis/OauthApi.groovy
+++ keyworker-specs/src/test/groovy/uk/gov/justice/digital/hmpps/keyworker/mockapis/OauthApi.groovy
@@ -1,15 +1,7 @@
 package uk.gov.justice.digital.hmpps.keyworker.mockapis
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule
-import groovy.json.JsonBuilder
 import groovy.json.JsonOutput
-import uk.gov.justice.digital.hmpps.keyworker.mockapis.mockResponses.CaseNoteUsageResponse
-import uk.gov.justice.digital.hmpps.keyworker.mockapis.mockResponses.OffenderAssessmentsResponse
-import uk.gov.justice.digital.hmpps.keyworker.mockapis.mockResponses.OffenderSearchResponse
-import uk.gov.justice.digital.hmpps.keyworker.mockapis.mockResponses.OffenderSentencesResponse
-import uk.gov.justice.digital.hmpps.keyworker.model.AgencyLocation
-import uk.gov.justice.digital.hmpps.keyworker.model.Caseload
-import uk.gov.justice.digital.hmpps.keyworker.model.Location
 import uk.gov.justice.digital.hmpps.keyworker.model.UserAccount
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*
@@ -41,7 +33,7 @@ class OauthApi extends WireMockRule {
         }
 
         this.stubFor(
-                post('/oauth/token')
+                post('/auth/oauth/token')
                         .withHeader('authorization', equalTo('Basic ZWxpdGUyYXBpY2xpZW50OmNsaWVudHNlY3JldA=='))
                         .withHeader('Content-Type', equalTo('application/x-www-form-urlencoded'))
                         .withRequestBody(equalTo("username=${user.username}&password=password&grant_type=password"))
@@ -51,7 +43,7 @@ class OauthApi extends WireMockRule {
 
     void stubInvalidOAuthTokenRequest(UserAccount user, boolean badPassword = false) {
         this.stubFor(
-                post('/oauth/token')
+                post('/auth/oauth/token')
                         .withHeader('authorization', equalTo('Basic ZWxpdGUyYXBpY2xpZW50OmNsaWVudHNlY3JldA=='))
                         .withHeader('Content-Type', equalTo('application/x-www-form-urlencoded'))
                         .withRequestBody(matching("username=${user.username}&password=.*&grant_type=password"))
