1c20956713758a54512dbd56895a942cfdf2bd5d
John Robertson <jsrobertson@gmail.com>
2018-10-15 10:25:34 +0100

    NN-1237 Moved out change paragraph into own component

--- src/Statistic/Statistic.js
+++ src/Statistic/Statistic.js
@@ -1,49 +1,25 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Heading, Value, Change } from './Statistic.styles';
+import StatisticChange from './StatisticChange';
 
-class Statistic extends Component {
-  constructor (props) {
-    super(props);
-  }
-
-  renderChangeString = () => {
-    const { change } = this.props;
-    if (!change || change.value === 0) return `no change since last ${change.period}`;
-
-    const { value, period } = change;
-    const changeType = value > 0 ? 'increase' : 'decrease';
-
-    return (
-      <Fragment>
-        {changeType === 'increase' && '+'}
-        {value}
-        {value !== 0 && (
-          <img src={`/images/icon-${changeType}.png`} alt={changeType} height={20} width={20} />
-        )}
-        {period && `since last ${period}`}
-      </Fragment>
-    );
-  };
-
-  render () {
-    return (
-      <Container>
-        <Heading>{this.props.heading}</Heading>
-        <Value>{this.props.value}</Value>
-        <Change>{this.renderChangeString()}</Change>
-      </Container>
-    );
-  }
-}
+const Statistic = ({ heading, value, change }) => {
+  return (
+    <Container>
+      <Heading>{heading}</Heading>
+      <Value>{value}</Value>
+      <Change>
+        {change.value === 0 && `no change since last ${change.period}`}
+        {change.value !== 0 && <StatisticChange change={change} />}
+      </Change>
+    </Container>
+  );
+};
 
 Statistic.propTypes = {
   heading: PropTypes.string,
   value: PropTypes.number,
-  change: PropTypes.shape({
-    value: PropTypes.number,
-    period: PropTypes.string
-  })
+  change: PropTypes.object
 };
 
 Statistic.defaultProps = {
---
+++ src/Statistic/StatisticChange.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+const StatisticChange = ({ change }) => {
+  const { value, period } = change;
+  const changeType = value > 0 ? 'increase' : 'decrease';
+
+  return (
+    <Fragment>
+      {changeType === 'increase' && '+'}
+      {value}
+      <img src={`/images/icon-${changeType}.png`} alt={changeType} height={20} width={20} />
+      {period && `since last ${period}`}
+    </Fragment>
+  );
+};
+
+StatisticChange.propTypes = {
+  change: PropTypes.shape({
+    value: PropTypes.number,
+    period: PropTypes.string
+  })
+};
+
+export default StatisticChange;
