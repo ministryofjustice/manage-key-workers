93bffa20537031ecf8a7036ecaedd7ef42f2fb3d
John Robertson <jsrobertson@gmail.com>
2018-10-19 22:10:02 +0100

    NN-1285 Removed no-unneeded-ternary override

--- backend/controllers/userSearch.js
+++ backend/controllers/userSearch.js
@@ -1,31 +1,30 @@
-const asyncMiddleware = require('../middleware/asyncHandler');
-const log = require('../log');
+const asyncMiddleware = require("../middleware/asyncHandler");
+const log = require("../log");
 
-const search = function (eliteApi, res, agencyId, nameFilter, roleFilter) {
-  return eliteApi.userSearch(
-    res.locals,
-    {
-      agencyId,
-      nameFilter,
-      roleFilter: roleFilter ? roleFilter : ''
-    });
+const search = function(eliteApi, res, agencyId, nameFilter, roleFilter) {
+  return eliteApi.userSearch(res.locals, {
+    agencyId,
+    nameFilter,
+    roleFilter: roleFilter || ""
+  });
 };
 
-const adminSearch = function (eliteApi, res, agencyId, nameFilter, roleFilter) {
-  return eliteApi.userSearchAdmin(
-    res.locals,
-    {
-      agencyId,
-      nameFilter,
-      roleFilter: roleFilter ? roleFilter : ''
-    });
+const adminSearch = function(eliteApi, res, agencyId, nameFilter, roleFilter) {
+  return eliteApi.userSearchAdmin(res.locals, {
+    agencyId,
+    nameFilter,
+    roleFilter: roleFilter || ""
+  });
 };
 
-const userSearchFactory = (eliteApi) => {
+const userSearchFactory = eliteApi => {
   const userSearch = asyncMiddleware(async (req, res) => {
     const { agencyId, nameFilter, roleFilter, hasAdminRole } = req.query;
-    log.debug('Performing user search.  Admin role=' + hasAdminRole);
-    const response = hasAdminRole === 'true' ? await adminSearch(eliteApi, res, agencyId, nameFilter, roleFilter) : await search(eliteApi, res, agencyId, nameFilter, roleFilter);
+    log.debug("Performing user search.  Admin role=" + hasAdminRole);
+    const response =
+      hasAdminRole === "true"
+        ? await adminSearch(eliteApi, res, agencyId, nameFilter, roleFilter)
+        : await search(eliteApi, res, agencyId, nameFilter, roleFilter);
     res.set(res.locals.responseHeaders);
     res.json(response);
   });
