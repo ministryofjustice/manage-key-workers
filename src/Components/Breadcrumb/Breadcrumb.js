f43830b7b622449b06458e4e437135e3f90c8f5f
John Robertson <jsrobertson@gmail.com>
2018-12-14 13:13:39 +0000

    NN-1498 Added breadcrumb tests

--- src/Components/Breadcrumb/Breadcrumb.js
+++ src/Components/Breadcrumb/Breadcrumb.js
@@ -5,27 +5,9 @@ import withBreadcrumbs from 'react-router-breadcrumbs-hoc'
 import links from '../../links'
 import { BreadcrumbContainer, BreadcrumbList, BreadcrumbListItem } from './Breadcrumb.styles'
 import { routeMatchType } from '../../types'
+import routes from '../../routes'
 
-// This needs to be put into a centralised route config that is also used in App.js
-// See: https://reacttraining.com/react-router/web/example/route-config
-const routes = [
-  { path: '/', breadcrumb: 'Manage key workers' },
-  { path: '/unallocated', breadcrumb: 'Auto-allocate key workers' },
-  { path: '/unallocated/provisional-allocation', breadcrumb: 'Suggested key worker allocation' },
-  { path: '/key-worker-statistics', breadcrumb: 'Key worker statistics' },
-  { path: '/key-worker-search', breadcrumb: 'Search for a key worker' },
-  { path: '/key-worker', breadcrumb: null },
-  { path: '/key-worker/:staffId/confirm-edit', breadcrumb: 'Edit' },
-  { path: '/key-worker/:staffId', breadcrumb: 'Key worker profile' },
-  { path: '/offender-history', breadcrumb: null },
-  { path: '/offender-history/:offenderId', breadcrumb: 'Offender key worker allocation history' },
-  { path: '/give-nomis-access', breadcrumb: 'Give access to New NOMIS' },
-  { path: '/maintain-roles/:staffId/roles', breadcrumb: 'Current profile roles' },
-  { path: '/maintain-roles/search-results', breadcrumb: 'Results' },
-  { path: '/maintain-roles/:staffId', breadcrumb: null },
-]
-
-const Breadcrumb = ({ breadcrumbs, match }) => (
+export const Breadcrumb = ({ breadcrumbs, match }) => (
   <BreadcrumbContainer>
     <BreadcrumbList>
       <BreadcrumbListItem>
