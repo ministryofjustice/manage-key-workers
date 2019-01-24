b80e39d50a39444c6cc2b01a09d6fe8249a52006
John Robertson <jsrobertson@gmail.com>
2019-01-24 13:08:54 +0000

    NN-1539 Restored old datepicker

--- src/DatePickerInput/datePicker.js
+++ src/DatePickerInput/datePicker.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import Datetime from 'react-datetime'
 import 'react-datetime/css/react-datetime.css'
 import PropTypes from 'prop-types'
@@ -10,15 +10,12 @@ class DatePicker extends Component {
   }
 
   renderInput(props) {
-    const { inputId, className, name, title } = this.props
+    const { inputId, className, name } = this.props
 
     return (
-      <Fragment>
-        <label htmlFor={name} className="form-label">
-          {title}
-        </label>
-        <input id={inputId} className={className} name={name} {...props} />
-      </Fragment>
+      <div>
+        <input id={inputId} className={className} name={name} {...props} readOnly />
+      </div>
     )
   }
 
@@ -45,14 +42,10 @@ class DatePicker extends Component {
 DatePicker.propTypes = {
   handleDateChange: PropTypes.func.isRequired,
   shouldShowDay: PropTypes.func.isRequired,
-  className: PropTypes.string,
+  className: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   inputId: PropTypes.string.isRequired,
-}
-
-DatePicker.defaultProps = {
-  className: null,
 }
 
 export default DatePicker
--- src/DatePickerInput/index.js
+++ src/DatePickerInput/index.js
@@ -4,23 +4,20 @@ import PropTypes from 'prop-types'
 import DatePicker from './datePicker'
 
 const DatePickerInput = props => {
-  const { additionalClassName, handleDateChange, customValidation, title } = props
-  const validation = date => {
-    if (customValidation) return customValidation(date)
-
-    return !date.isBefore(
-      moment()
-        .add(1, 'days')
-        .startOf('day')
-    )
-  }
+  const { additionalClassName, handleDateChange } = props
 
   return (
     <DatePicker
       inputProps={{ className: `datePickerInput form-control ${additionalClassName}` }}
       name="date"
-      shouldShowDay={validation}
-      title={title}
+      shouldShowDay={date =>
+        !date.isBefore(
+          moment()
+            .add(1, 'days')
+            .startOf('day')
+        )
+      }
+      title="Date"
       handleDateChange={handleDateChange}
       {...props}
     />
@@ -28,15 +25,7 @@ const DatePickerInput = props => {
 }
 
 DatePickerInput.propTypes = {
-  additionalClassName: PropTypes.string,
+  additionalClassName: PropTypes.string.isRequired,
   handleDateChange: PropTypes.func.isRequired,
-  customValidation: PropTypes.func,
-  title: PropTypes.string,
-}
-
-DatePickerInput.defaultProps = {
-  additionalClassName: '',
-  customValidation: null,
-  title: 'Date',
 }
 export default DatePickerInput
