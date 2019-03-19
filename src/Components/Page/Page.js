083a8d174c87e8dbdb89ae6683efe330a0da0b33
Jamie Draper <draper65@googlemail.com>
2019-03-19 14:08:18 +0000

    Revert "Revert "NN-1724 Revert name change""

--- src/Components/Page/Page.js
+++ src/Components/Page/Page.js
@@ -25,7 +25,7 @@ export class Page extends Component {
   }
 
   renderTitleString = title => {
-    document.title = `${title} - Key worker - Digital Prison Services`
+    document.title = `${title} - Key worker - Prison NOMIS`
   }
 
   render() {
