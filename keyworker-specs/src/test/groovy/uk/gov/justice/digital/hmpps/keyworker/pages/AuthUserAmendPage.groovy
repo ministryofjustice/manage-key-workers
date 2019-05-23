1e32d748f9c7b5464db0efd85cf339106deaac3c
Peter Phillips <peter@greenthistle.co.uk>
2019-05-23 15:32:22 +0100

    NN-1885: Ooops missed out file in commit

---
+++ keyworker-specs/src/test/groovy/uk/gov/justice/digital/hmpps/keyworker/pages/AuthUserAmendPage.groovy
@@ -0,0 +1,24 @@
+package uk.gov.justice.digital.hmpps.keyworker.pages
+
+import geb.Page
+
+public class AuthUserAmendPage extends Page {
+    static url = "/admin-utilities/maintain-auth-users/"
+
+    static at = {
+        browser.currentUrl.contains(url)
+        headingText.contains('Amend auth user: ')
+    }
+
+    static content = {
+        headingText { $('h1').text() }
+        amendButton { $('[data-qa="amend-button"]') }
+        errorSummary(required: false) { $('#error-summary') }
+    }
+
+    void amendUser(String email) {
+        $('form').email = email
+        assert amendButton.text() == 'Amend'
+        amendButton.click()
+    }
+}
