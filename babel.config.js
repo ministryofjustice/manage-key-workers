4d91c8b0bc5869c8f870b9780e54917fc056f0de
Nevelina Aleksandrova <nevelina@diginaga.online>
2019-12-02 09:26:26 +0000

    NN-2309: Fix jest vulnerabilities

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,8 @@ const plugins = [
   ['@babel/plugin-proposal-decorators', { legacy: true }],
   ['@babel/plugin-proposal-class-properties', { loose: true }],
   '@babel/plugin-proposal-throw-expressions',
+  '@babel/plugin-transform-runtime',
+  '@babel/plugin-transform-regenerator',
 ]
 
 const ignore = ['.spec.js', '.test.js', '-test.js', '/__tests__/']
