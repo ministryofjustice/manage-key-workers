0187c60c17764ec87eaf4857afa484a87732678e
Peter Phillips <peter@greenthistle.co.uk>
2020-06-26 08:54:09 +0100

    DT-951: Disable webpack and remove manifest

---
+++ backend/apis.js
@@ -0,0 +1,33 @@
+const config = require('./config')
+const clientFactory = require('./api/oauthEnabledClient')
+const { elite2ApiFactory } = require('./api/elite2Api')
+const { oauthApiFactory } = require('./api/oauthApi')
+const { keyworkerApiFactory } = require('./api/keyworkerApi')
+
+const elite2Api = elite2ApiFactory(
+  clientFactory({
+    baseUrl: config.apis.elite2.url,
+    timeout: config.apis.elite2.timeoutSeconds * 1000,
+  })
+)
+
+const oauthApi = oauthApiFactory(
+  clientFactory({
+    baseUrl: config.apis.oauth2.url,
+    timeout: config.apis.oauth2.timeoutSeconds * 1000,
+  }),
+  { ...config.apis.oauth2 }
+)
+
+const keyworkerApi = keyworkerApiFactory(
+  clientFactory({
+    baseUrl: config.apis.keyworker.url,
+    timeout: 1000 * config.apis.keyworker.timeoutSeconds,
+  })
+)
+
+module.exports = {
+  elite2Api,
+  oauthApi,
+  keyworkerApi,
+}
