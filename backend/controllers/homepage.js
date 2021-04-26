199f61feaa3938e21aff0c3d49449f67b1bea554
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-04-26 14:25:01 +0100

    NN-3321 hide manage key worker link behind the migrated flag

--- backend/controllers/homepage.js
+++ backend/controllers/homepage.js
@@ -62,6 +62,8 @@ module.exports = ({ keyworkerApi, oauthApi }) => async (req, res) => {
     keyworkerApi.getPrisonMigrationStatus(res.locals, activeCaseLoadId),
   ])
 
+  if (!prisonStatus?.supported) return res.redirect('/not-found')
+
   const roleCodes = currentRoles.map((userRole) => userRole.roleCode)
 
   return res.render('homepage', {
