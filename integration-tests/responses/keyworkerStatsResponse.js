e50a9780d5d2e8500544b03ff7e7ff73aeddb45d
Richard Popple <richard.popple@digital.justice.gov.uk>
2021-09-21 17:00:23 +0100

    NN-3313 removing seperate data structures as they are now in the specs

--- integration-tests/responses/keyworkerStatsResponse.js
+++
@@ -1,8 +0,0 @@
-const keyworkerStatsResponse = {
-  caseNoteEntryCount: 10,
-  caseNoteSessionCount: 10,
-  complianceRate: 0,
-  projectedKeyworkerSessions: 0,
-}
-
-export default keyworkerStatsResponse
