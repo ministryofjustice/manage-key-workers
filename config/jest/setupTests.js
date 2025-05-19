57a80e03b3d9b8d06db01bfde70d8671434bc1cf
bill-moj <167539377+bill-moj@users.noreply.github.com>
2025-05-19 10:45:24 +0100

    fix some of the package security audit issues (#812)
    * fix some of the package security audit issues

--- config/jest/setupTests.js
+++ config/jest/setupTests.js
@@ -1,4 +1,23 @@
+import { TextEncoder, TextDecoder } from 'util'
 import { configure } from 'enzyme'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+// Polyfill for TextEncoder
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = TextEncoder
+}
+if (typeof window.TextEncoder === 'undefined') {
+  window.TextEncoder = global.TextEncoder
+}
+
+// Polyfill for TextDecoder
+if (typeof global.TextDecoder === 'undefined') {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  global.TextDecoder = TextDecoder
+}
+if (typeof window.TextDecoder === 'undefined') {
+  window.TextDecoder = global.TextDecoder
+}
 
 configure({ adapter: new Adapter() })
