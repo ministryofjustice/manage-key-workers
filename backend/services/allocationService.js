49491170dfbfe1cc9f1cc9c92aa589de53bbc3ba
JackReeveMoJ <jack.reeve@digital.justice.gov.uk>
2025-06-27 13:55:28 +0100

    MIA-537: Fix auto allocation over capacity message (#819)

--- backend/services/allocationService.js
+++ backend/services/allocationService.js
@@ -107,16 +107,10 @@ const serviceFactory = (elite2Api, prisonerSearchApi, keyworkerApi, offenderSear
   }
 
   function warning(error) {
-    if (error.response && error.response.body) {
-      const msg = error.response.body.userMessage
-      if (
-        msg === 'No Key workers available for allocation.' ||
-        msg === 'All available Key workers are at full capacity.'
-      ) {
-        return msg
-      }
-    }
-    return null
+    const devMsg = error.response?.body?.developerMessage
+    const userMsg = error.response?.body?.userMessage
+    const filter = ['No Key workers available for allocation.', 'All available Key workers are at full capacity.']
+    return filter.find((msg) => devMsg?.includes(msg) || userMsg?.includes(msg))
   }
 
   const findKeyworkerStaffIdForOffender = (offenderKeyworkers, offenderNo) =>
@@ -211,7 +205,7 @@ const serviceFactory = (elite2Api, prisonerSearchApi, keyworkerApi, offenderSear
     return {
       keyworkerResponse: availableKeyworkers,
       allocatedResponse:
-        offenderWithAllocatedKeyworkerDtos.length > 0
+        offenderWithAllocatedKeyworkerDtos?.length > 0
           ? await offendersWithKeyworkersGettingPrisoners(
               context,
               offenderWithAllocatedKeyworkerDtos,
