fafea35030b17d56e7c3a1d089e30135b76d35c0
bill-moj <bill.yeung@digital.justice.gov.uk>
2025-07-04 14:09:44 +0100

    Publish announcement for coming updates

--- backend/homepageAnnouncement.js
+++ backend/homepageAnnouncement.js
@@ -1,11 +1,29 @@
+const launchSoonAnnouncement = {
+  title: 'An updated Key Worker service will launch soon',
+  html: 'You can find out more on <a href="https://justiceuk.sharepoint.com/sites/prisons-digital/SitePages/Keyworker.aspx">Sharepoint</a>.',
+}
+
 // eslint-disable-next-line import/prefer-default-export
 export const homepageAnnouncement = {
   DEV: {
-    MDI: {
-      title: 'An updated Key Worker service will launch soon',
-      html: 'You can find out more on <a href="#">Sharepoint</a>.',
-    },
+    MDI: launchSoonAnnouncement,
   },
-  'PRE-PRODUCTION': {},
-  PRODUCTION: {},
+  'PRE-PRODUCTION': {
+    BCI: launchSoonAnnouncement,
+    HII: launchSoonAnnouncement,
+    LFI: launchSoonAnnouncement,
+    LPI: launchSoonAnnouncement,
+    PNI: launchSoonAnnouncement,
+    RSI: launchSoonAnnouncement,
+    WMI: launchSoonAnnouncement,
+  },
+  PRODUCTION: {
+    BCI: launchSoonAnnouncement,
+    HII: launchSoonAnnouncement,
+    LFI: launchSoonAnnouncement,
+    LPI: launchSoonAnnouncement,
+    PNI: launchSoonAnnouncement,
+    RSI: launchSoonAnnouncement,
+    WMI: launchSoonAnnouncement,
+  },
 }
