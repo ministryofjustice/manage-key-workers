f06f5053700dffda4f8a1070d9000e5c7b63af9a
bill-moj <bill.yeung@digital.justice.gov.uk>
2025-07-09 11:27:54 +0100

    Minor tweak to launch announcement banner

--- backend/homepageAnnouncement.js
+++ backend/homepageAnnouncement.js
@@ -1,6 +1,6 @@
 const launchSoonAnnouncement = {
   title: 'An updated Key Worker service will launch soon',
-  html: 'You can find out more on <a href="https://justiceuk.sharepoint.com/sites/prisons-digital/SitePages/Keyworker.aspx">Sharepoint</a>.',
+  html: 'You can find out more on <a target="_blank" href="https://justiceuk.sharepoint.com/sites/prisons-digital/SitePages/Keyworker.aspx">SharePoint</a>.',
 }
 
 // eslint-disable-next-line import/prefer-default-export
