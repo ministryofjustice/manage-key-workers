bd0fde1b9e79955d10d4a413e2701ff577a093e5
bill-moj <bill.yeung@digital.justice.gov.uk>
2025-07-18 18:20:18 +0100

    Update announcement content

--- backend/homepageAnnouncement.js
+++ backend/homepageAnnouncement.js
@@ -1,6 +1,6 @@
 const launchSoonAnnouncement = {
-  title: 'An updated Key Worker service will launch soon',
-  html: 'You can find out more on <a target="_blank" href="https://justiceuk.sharepoint.com/sites/prisons-digital/SitePages/Keyworker.aspx">SharePoint</a>.',
+  title: 'You should use the updated Key Worker service',
+  html: `You can access this using <a href="${process.env.ALLOCATIONS_UI_URL}">this link</a>. You can find out more on <a target="_blank" href="https://justiceuk.sharepoint.com/sites/prisons-digital/SitePages/Keyworker.aspx">SharePoint</a>.`,
 }
 
 // eslint-disable-next-line import/prefer-default-export
