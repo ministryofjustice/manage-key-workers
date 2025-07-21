78c04dc7b298d5aacd389e6980123eeac7d649f1
bill-moj <bill.yeung@digital.justice.gov.uk>
2025-07-21 09:16:01 +0100

    Address PR comments

--- backend/homepageAnnouncement.js
+++ backend/homepageAnnouncement.js
@@ -1,4 +1,9 @@
 const launchSoonAnnouncement = {
+  title: 'An updated Key Worker service will launch soon',
+  html: 'You can find out more on <a target="_blank" href="https://justiceuk.sharepoint.com/sites/prisons-digital/SitePages/Keyworker.aspx">SharePoint</a>.',
+}
+
+const launchedAnnouncement = {
   title: 'You should use the updated Key Worker service',
   html: `You can access this using <a href="${process.env.ALLOCATIONS_UI_URL}">this link</a>. You can find out more on <a target="_blank" href="https://justiceuk.sharepoint.com/sites/prisons-digital/SitePages/Keyworker.aspx">SharePoint</a>.`,
 }
@@ -6,16 +11,16 @@ const launchSoonAnnouncement = {
 // eslint-disable-next-line import/prefer-default-export
 export const homepageAnnouncement = {
   DEV: {
-    MDI: launchSoonAnnouncement,
+    MDI: launchedAnnouncement,
   },
   'PRE-PRODUCTION': {
-    BCI: launchSoonAnnouncement,
-    HII: launchSoonAnnouncement,
-    LFI: launchSoonAnnouncement,
-    LPI: launchSoonAnnouncement,
-    PNI: launchSoonAnnouncement,
-    RSI: launchSoonAnnouncement,
-    WMI: launchSoonAnnouncement,
+    BCI: launchedAnnouncement,
+    HII: launchedAnnouncement,
+    LFI: launchedAnnouncement,
+    LPI: launchedAnnouncement,
+    PNI: launchedAnnouncement,
+    RSI: launchedAnnouncement,
+    WMI: launchedAnnouncement,
   },
   PRODUCTION: {
     BCI: launchSoonAnnouncement,
